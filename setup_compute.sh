af7493c7637b95f27f714383c93433949eb4fb1f
Mike Metral <metral@gmail.com>
2012-09-11 12:27:24 -0500

    Added comments, removed /proc from find cmd, & cleaned up code

--- setup_compute.sh
+++ setup_compute.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 #-------------------------------------------------------------------------------
-# fix permissions
+# Fixes the compute node to support live-migration
+#-------------------------------------------------------------------------------
 
-for a in libvirt-bin nova-network nova-compute nova-vncproxy ; do sudo service "$a" stop; done
+# Disable OpenStack & related services
+for a in libvirt-bin nova-network nova-compute nova-vncproxy ; \
+    do sudo service "$a" stop; done
 killall -9 dnsmasq
 
-orig_libvirtd_guid=`getent group kvm | cut -d ":" -f3`
-new_libvirtd_guid=2001
+# Store & set old and new uid & gid's for:
+# libvirtd, nova, libvirt-dnsmasq & libvirt-qemu
+orig_libvirtd_gid=`getent group libvirtd | cut -d ":" -f3`
+new_libvirtd_gid=2000
 
 orig_nova_uid=`id -u nova`
-orig_nova_guid=`id -g nova`
-new_nova_uid=2000
-new_nova_guid=2000
+orig_nova_gid=`id -g nova`
+new_nova_uid=2001
+new_nova_gid=2001
 
 orig_libvirt_dnsmasq_uid=`id -u libvirt-dnsmasq`
 new_libvirt_dnsmasq_uid=2002
 
-orig_kvm_guid=`getent group kvm | cut -d ":" -f3`
-new_kvm_guid=2003
+orig_kvm_gid=`getent group kvm | cut -d ":" -f3`
+new_kvm_gid=2003
 
 orig_libvirt_qemu_uid=`id -u libvirt-qemu`
 new_libvirt_qemu_uid=2004
@@ -30,36 +35,46 @@ sudo userdel libvirt-dnsmasq
 sudo groupdel libvirtd
 sudo groupdel kvm
 
-# Fix libvirtd
-sudo groupadd -g $new_libvirtd_guid libvirtd
-find / -gid $orig_libvirtd_guid -exec chgrp -h $new_libvirtd_guid '{}' \+
+# Fix libvirtd group & old permissions
+sudo groupadd -g $new_libvirtd_gid libvirtd
+find / -type d -name proc -prune -o -gid $orig_libvirtd_gid \
+    -exec chgrp -h $new_libvirtd_gid '{}' \+
 
-# Fix nova
-sudo groupadd -g $new_nova_guid nova
-sudo useradd -u $new_nova_uid -g $new_nova_guid -d /var/lib/nova -s /bin/false nova
+# Fix nova user & group & old permissions
+sudo groupadd -g $new_nova_gid nova
+sudo useradd -u $new_nova_uid -g $new_nova_gid \
+    -d /var/lib/nova -s /bin/false nova
 sudo usermod -a -G libvirtd nova
-find / -uid $orig_nova_uid -exec chown -h $new_nova_uid '{}' \+
-find / -gid $orig_nova_guid -exec chgrp -h $new_nova_guid '{}' \+
+find / -type d -name proc -prune -o -uid $orig_nova_uid \
+    -exec chown -h $new_nova_uid '{}' \+
+find / -type d -name proc -prune -o -gid $orig_nova_gid \
+    -exec chgrp -h $new_nova_gid '{}' \+
 
-# Fix libvirt-dnsmasq
-sudo useradd -u $new_libvirt_dnsmasq_uid -g $new_libvirtd_guid -d /var/lib/libvirt/dnsmasq -s /bin/false libvirt-dnsmasq
-find / -uid $orig_libvirt_dnsmasq_uid -exec chown -h $new_libvirt_dnsmasq_uid '{}' \+
+# Fix libvirt-dnsmasq user & old permissions
+sudo useradd -u $new_libvirt_dnsmasq_uid -g $new_libvirtd_gid \
+    -d /var/lib/libvirt/dnsmasq -s /bin/false libvirt-dnsmasq
+find / -type d -name proc -prune -o -uid $orig_libvirt_dnsmasq_uid \
+    -exec chown -h $new_libvirt_dnsmasq_uid '{}' \+
 
-# Fix kvm
-sudo groupadd -g $new_kvm_guid kvm
-find / -gid $orig_kvm_guid -exec chgrp -h $new_kvm_guid '{}' \+
+# Fix kvm & old permissions
+sudo groupadd -g $new_kvm_gid kvm
+find / -type d -name proc -prune -o -gid $orig_kvm_gid \
+    -exec chgrp -h $new_kvm_gid '{}' \+
 
-# Fix libvirt-qemu
-sudo useradd -u $new_libvirt_qemu_uid -g $new_kvm_guid -d /var/lib/libvirt -s /bin/false libvirt-qemu
-find / -uid $orig_libvirt_qemu_uid -exec chown -h $new_libvirt_qemu_uid '{}' \+
+# Fix libvirt-qemu & old permissions
+sudo useradd -u $new_libvirt_qemu_uid -g $new_kvm_gid \
+    -d /var/lib/libvirt -s /bin/false libvirt-qemu
+find / -type d -name proc -prune -o -uid $orig_libvirt_qemu_uid \
+    -exec chown -h $new_libvirt_qemu_uid '{}' \+
 
 #-------------------------------------------------------------------------------
 
-# Setup to use existing remote NFS as back-end storage for instances
-REMOTE_NFS_IP="10.80.1.46"
+# Remote (on the controller) NFS server settings
+REMOTE_NFS_IP=$1
 REMOTE_NFS_MOUNT="/var/lib/nova/instances"
 LOCAL_NFS_MOUNT="/var/lib/nova/instances"
 
+# Install & mount NFS server
 sudo apt-get update; sudo apt-get install nfs-common -y
 sudo mount $REMOTE_NFS_IP:$REMOTE_NFS_MOUNT $LOCAL_NFS_MOUNT
 sudo chown -R nova:nova /var/lib/nova/instances
@@ -73,19 +88,22 @@ listen_tcp = 1
 auth_tcp = 'none'
 EOF
 
-# Configure libvirt-bin.conf options
-sudo sed -i "s/env libvirtd_opts=\"-d\"/env libvirtd_opts=\"-d -l\"/g" /etc/init/libvirt-bin.conf
-sudo sed -i "s/libvirtd_opts=\"-d\"/libvirtd_opts=\"-d -l\"/g" /etc/default/libvirt-bin
+# Configure libvirt-bin.conf & libvirt-bin options
+sudo sed -i "s/env libvirtd_opts=\"-d\"/env libvirtd_opts=\"-d -l\"/g" \
+    /etc/init/libvirt-bin.conf
+sudo sed -i "s/libvirtd_opts=\"-d\"/libvirtd_opts=\"-d -l\"/g" \
+    /etc/default/libvirt-bin
 
-# Modify nova.conf to have vncserver not listen on a specific ip
-sudo sed -i 's/--vncserver_listen=.*$/#--vncserver_listen=0.0.0.0/g' /etc/nova/nova.conf
+# Modify nova.conf to not listen on a specific local vncserver
+sudo sed -i 's/--vncserver_listen=.*$/#--vncserver_listen=0.0.0.0/g' \
+    /etc/nova/nova.conf
 
 # Restart libvirt-bin
 sudo /etc/init.d/libvirt-bin stop
 sudo /etc/init.d/libvirt-bin start
 
-# Add automount
+# Mount NFS server on boot
 (cat | sudo tee -a /etc/fstab ) << EOF
-$REMOTE_NFS_IP:$REMOTE_NFS_MOUNT 	$LOCAL_NFS_MOUNT    nfs    rw      0        0
+$REMOTE_NFS_IP:$REMOTE_NFS_MOUNT $LOCAL_NFS_MOUNT   nfs   defaults    0     0
 EOF
 #-------------------------------------------------------------------------------
--- setup_controller.sh
+++ setup_controller.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 #-------------------------------------------------------------------------------
-# fix permissions
+# Fixes the controller node to support live-migration
+#-------------------------------------------------------------------------------
 
-for a in keystone libvirt-bin glance rabbitmq-server nova-cert nova-consoleauth nova-api nova-network nova-scheduler nova-vncproxy nova-volume; do sudo service "$a" stop; done
+# Disable OpenStack & related services
+for a in keystone libvirt-bin glance rabbitmq-server nova-cert \
+    nova-consoleauth nova-api nova-network nova-scheduler nova-vncproxy \
+    nova-volume; do sudo service "$a" stop; done
 killall -9 dnsmasq
 
-orig_libvirtd_guid=`getent group kvm | cut -d ":" -f3`
-new_libvirtd_guid=2001
+# Install libvirt-bin & kvm on the controller (in case they're not installed)
+sudo apt-get update ; sudo apt-get install libvirt-bin kvm -y
+
+# Store & set old and new uid & gid's for:
+# libvirtd, nova, libvirt-dnsmasq & libvirt-qemu
+orig_libvirtd_gid=`getent group libvirtd | cut -d ":" -f3`
+new_libvirtd_gid=2000
 
 orig_nova_uid=`id -u nova`
-orig_nova_guid=`id -g nova`
-new_nova_uid=2000
-new_nova_guid=2000
+orig_nova_gid=`id -g nova`
+new_nova_uid=2001
+new_nova_gid=2001
 
 orig_libvirt_dnsmasq_uid=`id -u libvirt-dnsmasq`
 new_libvirt_dnsmasq_uid=2002
 
-orig_kvm_guid=`getent group kvm | cut -d ":" -f3`
-new_kvm_guid=2003
+orig_kvm_gid=`getent group kvm | cut -d ":" -f3`
+new_kvm_gid=2003
 
 orig_libvirt_qemu_uid=`id -u libvirt-qemu`
 new_libvirt_qemu_uid=2004
@@ -30,32 +39,41 @@ sudo userdel libvirt-dnsmasq
 sudo groupdel libvirtd
 sudo groupdel kvm
 
-# Fix libvirtd
-sudo groupadd -g $new_libvirtd_guid libvirtd
-find / -gid $orig_libvirtd_guid -exec chgrp -h $new_libvirtd_guid '{}' \+
+# Fix libvirtd group & old permissions
+sudo groupadd -g $new_libvirtd_gid libvirtd
+find / -type d -name proc -prune -o -gid $orig_libvirtd_gid \
+    -exec chgrp -h $new_libvirtd_gid '{}' \+
 
-# Fix nova
-sudo groupadd -g $new_nova_guid nova
-sudo useradd -u $new_nova_uid -g $new_nova_guid -d /var/lib/nova -s /bin/false nova
+# Fix nova user & group & old permissions
+sudo groupadd -g $new_nova_gid nova
+sudo useradd -u $new_nova_uid -g $new_nova_gid \
+    -d /var/lib/nova -s /bin/false nova
 sudo usermod -a -G libvirtd nova
-find / -uid $orig_nova_uid -exec chown -h $new_nova_uid '{}' \+
-find / -gid $orig_nova_guid -exec chgrp -h $new_nova_guid '{}' \+
+find / -type d -name proc -prune -o -uid $orig_nova_uid \
+    -exec chown -h $new_nova_uid '{}' \+
+find / -type d -name proc -prune -o -gid $orig_nova_gid \
+    -exec chgrp -h $new_nova_gid '{}' \+
 
-# Fix libvirt-dnsmasq
-sudo useradd -u $new_libvirt_dnsmasq_uid -g $new_libvirtd_guid -d /var/lib/libvirt/dnsmasq -s /bin/false libvirt-dnsmasq
-find / -uid $orig_libvirt_dnsmasq_uid -exec chown -h $new_libvirt_dnsmasq_uid '{}' \+
+# Fix libvirt-dnsmasq user & old permissions
+sudo useradd -u $new_libvirt_dnsmasq_uid -g $new_libvirtd_gid \
+    -d /var/lib/libvirt/dnsmasq -s /bin/false libvirt-dnsmasq
+find / -type d -name proc -prune -o -uid $orig_libvirt_dnsmasq_uid \
+    -exec chown -h $new_libvirt_dnsmasq_uid '{}' \+
 
-# Fix kvm
-sudo groupadd -g $new_kvm_guid kvm
-find / -gid $orig_kvm_guid -exec chgrp -h $new_kvm_guid '{}' \+
+# Fix kvm & old permissions
+sudo groupadd -g $new_kvm_gid kvm
+find / -type d -name proc -prune -o -gid $orig_kvm_gid \
+    -exec chgrp -h $new_kvm_gid '{}' \+
 
-# Fix libvirt-qemu
-sudo useradd -u $new_libvirt_qemu_uid -g $new_kvm_guid -d /var/lib/libvirt -s /bin/false libvirt-qemu
-find / -uid $orig_libvirt_qemu_uid -exec chown -h $new_libvirt_qemu_uid '{}' \+
+# Fix libvirt-qemu & old permissions
+sudo useradd -u $new_libvirt_qemu_uid -g $new_kvm_gid \
+    -d /var/lib/libvirt -s /bin/false libvirt-qemu
+find / -type d -name proc -prune -o -uid $orig_libvirt_qemu_uid \
+    -exec chown -h $new_libvirt_qemu_uid '{}' \+
 
 #-------------------------------------------------------------------------------
-# setup nfs
 
+# Set local NFS server mount point
 NFS_MOUNT=/var/lib/nova/instances
 
 # Install NFS server
